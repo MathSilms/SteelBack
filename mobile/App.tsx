cecf7bc0ef082bf7ddc91cda8497fc79d7d9eae7
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-03-16 18:14:47 -0300

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,8 +7,6 @@ import {Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
 
-//45:57
-
 import Routes from './src/routes'
 
 export default function App() {
